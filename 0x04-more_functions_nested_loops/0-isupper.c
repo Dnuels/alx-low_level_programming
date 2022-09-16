df87773598ba525b1af8b08776c1d6f1dd693ba8
dnuels <chidiswhite@gmail.com>
2022-09-16 11:58:07 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,15 +3,17 @@
 /**
  * main - check if character is uppercase or not
  * @c: character to be tested
- * Return: 1 whether it is, 0 otherwise
+ * Return: 1 whether it is, 0 others
  */
-
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c < 'Z'))
+	if ((c >= 65) && (c < 91))
 	{
 		return (1);
 	}
+	else
+	{
 
-	return (0);
+		return (0);
+	}
 }
